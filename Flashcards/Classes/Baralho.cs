6b6761787230bc4b07cc4b50be0beae4b3a32d8b
FilhoDeEd <edson.er.rodrigues@gmail.com>
2022-09-01 17:03:28 -0300

    build!: Criei as classes Baralho e Carta.

---
+++ Flashcards/Classes/Baralho.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Flashcards.Classes
+{
+    public class Baralho
+    {
+        public int IdBaralho { get; set; } //Deve ser único
+        public string Nome { get; set; }
+        public Carta[] Cartas { get; set; }
+
+        public Baralho(int idBaralho, string nome, Carta[] cartas)
+        {
+            IdBaralho = idBaralho;
+            Nome = nome;
+            Cartas = cartas;
+        }
+    }
+}
---
+++ Flashcards/Classes/Carta.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Flashcards.Classes
+{
+    public class Carta
+    {
+        public int IdCarta { get; set; } //Deve ser único
+        public string Pergunta { get; set; }
+        public string Resposta { get; set; }
+        public string Disciplina { get; set; }
+
+        public Carta(int idCarta, string pergunta, string resposta, string disciplina)
+        {
+            IdCarta = idCarta;
+            Pergunta = pergunta;
+            Resposta = resposta;
+            Disciplina = disciplina;
+        }
+    }
+}
